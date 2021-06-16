a87c73ca9fa4eef5b9572526ba7ea7b138ba0852
gryffindor_001 <daivesh000@gmail.com>
2021-06-16 20:28:01 +0530

    contests site linked

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ const app = express()
 
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, '../templates/views'))
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('', (req, res) => {
+    res.render('index')
+})
 
 app.get('/contests', (req, res) => {
     const url = 'https://kontests.net/api/v1/all'
