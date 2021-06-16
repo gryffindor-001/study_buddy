1b3e8c77da9d1dc36e3aa0a666e73c63fea4c0cd
gryffindor_001 <daivesh000@gmail.com>
2021-06-16 20:07:10 +0530

    added contests

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
 require('./db/mongoose')
+const express = require('express')
+const request = require('request')
+const hbs = require('hbs')
+const path = require('path')
+
+const app = express()
+
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, '../templates/views'))
+
+app.get('/contests', (req, res) => {
+    const url = 'https://kontests.net/api/v1/all'
+    
+    request({
+        url,
+        json: true
+    }, (error, contests) => {
+        if(error) {
+            return console.log('Errorrrr')
+        }
+
+        res.render('contests', {data: contests.body})
+    })
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Server is up')
+})
